4af3db6348e1234b7c35aee3fd7d6f79d6c7f1d6
fanil279 <fanilramakaev@gmail.com>
2025-09-16 15:30:07 +0500

    initial setup 2

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,30 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
 
 export default tseslint.config([
     globalIgnores(['dist']),
     {
         files: ['**/*.{ts,tsx}'],
         extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs['recommended-latest'],
-            reactRefresh.configs.vite,
+            // Enables recommended type-aware TypeScript linting rules to catch type errors early
+            ...tseslint.configs.recommendedTypeChecked,
+            // For stricter rules
+            ...tseslint.configs.strictTypeChecked,
+            // For stylistic rules
+            ...tseslint.configs.stylisticTypeChecked,
+
+            // Enable lint rules for React
+            reactX.configs['recommended-typescript'],
+            // Enable lint rules for React DOM
+            reactDom.configs.recommended,
         ],
         languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
+            parserOptions: {
+                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
     },
 ]);
